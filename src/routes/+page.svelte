c1a4c7fcf4e365e2387ad346d28b64dfd38ba34d
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-05-10 16:18:08 -0700

    get 3d rendering to work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,49 @@
 <script lang="ts">
-	const isOBSBrowserSource = typeof window !== 'undefined' && /OBS\/\d+\.\d+\.\d+/.test(window.navigator.userAgent);
+import { browser } from '$app/environment';
+import * as THREE from "three";
 
+if(browser) {
+	let camera: THREE.PerspectiveCamera;
+	let scene: THREE.Scene;
+	let renderer : THREE.WebGLRenderer;
+	let cube : THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>;
+
+	const init = () => {
+		scene = new THREE.Scene;
+		camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
+
+
+		renderer = new THREE.WebGLRenderer();
+		renderer.setSize( window.innerWidth, window.innerHeight);
+		document.body.appendChild( renderer.domElement );
+
+		const geometry = new THREE.BoxGeometry( 1,1,1,1 );
+		const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+		cube = new THREE.Mesh( geometry, material );
+		scene.add( cube );
+
+		camera.position.z = 5;
+	}
+
+	const render = () => {
+		renderer.clear()
+		renderer.render(scene, camera);
+	}
+
+	const animate = () => {
+		requestAnimationFrame(animate);
+
+		cube.rotation.x += 0.005;
+		cube.rotation.y += 0.005;
+
+		render();
+	}
+
+	init();
+	animate();
+}
+
+const isOBSBrowserSource = typeof window !== 'undefined' && /OBS\/\d+\.\d+\.\d+/.test(window.navigator.userAgent);
 </script>
 
 <h1>Welcome to SvelteKit</h1>

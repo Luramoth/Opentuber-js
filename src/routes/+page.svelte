c8aaab7cb3f39fc099189d7072ad7656af0857c9
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-05-10 14:46:12 -0700

    check OBS if app is being run in OBS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+	const isOBSBrowserSource = typeof window !== 'undefined' && /OBS\/\d+\.\d+\.\d+/.test(window.navigator.userAgent);
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1>is this being rendered in OBS?: {isOBSBrowserSource}</h1>

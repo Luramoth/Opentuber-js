b5a99d97c1414b199e3afb4967a2721884653062
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-05-10 23:58:40 -0700

    turn 3d renderer into a componant

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,60 +1,6 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
-	import { onMount } from 'svelte';
-	import { bubble } from 'svelte/internal';
-	import * as THREE from "three";
+	import ThreeCanvas from "$lib/threeCanvas.svelte";
 
-	const isOBSBrowserSource = typeof window !== 'undefined' && /OBS\/\d+\.\d+\.\d+/.test(window.navigator.userAgent);
-
-	let canvas: HTMLCanvasElement;
-
-	if(browser){
-		onMount(() => {
-			canvas = document.getElementById('3D') as HTMLCanvasElement;
-			canvas.setAttribute('alpha', 'true');
-
-			const renderer = new THREE.WebGLRenderer({canvas: canvas, alpha: true, antialias: true});
-			renderer.setSize(canvas.clientWidth, canvas.clientHeight);
-			renderer.setClearColor(0x000000, 0);
-
-			const camera = new THREE.PerspectiveCamera(
-				75,
-				canvas.clientWidth / canvas.clientHeight,
-				0.1,
-				1000
-			);
-			camera.position.z = 5;
-
-			const scene = new THREE.Scene();
-
-			const geometry = new THREE.BoxGeometry();
-			const material = new THREE.MeshBasicMaterial({color: 0xff00ff});
-			const cube = new THREE.Mesh(geometry, material);
-			scene.add(cube);
-
-			function animate() {
-				requestAnimationFrame(animate);
-
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
-
-				renderer.render(scene, camera);
-			}
-
-			animate();
-		});
-	}
 </script>
 
-<canvas id="3D"></canvas>
-
-<style>
-	canvas {
-		position: fixed;
-		top: 0px;
-		left: 0px;
-
-		width: 100%;
-		height: 100%;
-	}
-</style>
+<ThreeCanvas />
